e08b0330d610301feb2524bd273539306075a25a
mspagon <mspagon@gmail.com>
2017-11-20 08:38:58 -0600

    Added BaseActivity as the base functionality for an options menu. All Activities extend BaseActivity and therefore get this options menu.

---
+++ app/src/main/java/com/apt/hailingfrequencies/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.apt.hailingfrequencies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+// Base class inspired by
+// https://stackoverflow.com/questions/3270206/same-option-menu-in-all-activities-in-android/3270254#3270254
+
+public class BaseActivity extends AppCompatActivity {
+    // Populate the menu bar
+    // This is called by the system when the activity begins
+    // https://developer.android.com/guide/topics/ui/menus.html
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    // Event listener for menu items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                // do what you want here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/apt/hailingfrequencies/MainActivity.java
+++ app/src/main/java/com/apt/hailingfrequencies/MainActivity.java
@@ -2,12 +2,17 @@ package com.apt.hailingfrequencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
